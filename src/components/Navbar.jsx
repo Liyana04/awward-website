3a8fd3a0be7ff06b7bffdf2d95dfce399f10b027
Liyana04 <liyanaaris04@gmail.com>
2026-01-24 23:48:07 +0800

    first commit gsap tutorial awward winnnin stop at 1:26:31

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React, { useRef } from 'react'
-import { TiLocation } from 'react-icons/ti';
+import Button from './Button';
+import { TiLocationArrow } from "react-icons/ti";
+import gsap from 'gsap';
+import { useEffect } from 'react';
+
+const navItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
 
 const Navbar = () => {
-    const navContainer = useRef(null);  
+    const navContainerRef = useRef(null);  
+
+
   return (
     <div ref={navContainerRef} className='fixed insert-x-0 top-4 z-50 h-16 border-none transition-all
     duration-700 sm:inset-x-6'>
@@ -16,6 +23,19 @@ const Navbar = () => {
                         rightIcon={<TiLocationArrow />}
                         containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
                         />
+                    <div className='flex h-full items-center'>
+                        <div className='hidden md:block'>
+                            {navItems.map((item, index) => (
+                                <a
+                                key={index}
+                                href={`#${item.toLowerCase()}`}
+                                className="nav-hover-btn"
+                                >
+                                {item}
+                                </a>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </nav>
         </header>
